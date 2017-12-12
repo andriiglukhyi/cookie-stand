89d61b48e476161837969adf66a7dc0a30756e46
andriiglukhyi <andrigluxii@gmail.com>
2017-12-12 15:29:58 -0800

    broken sales.js

--- sales.js
+++ sales.js
@@ -1,10 +1,64 @@
 'use strict';
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; // via MDN docs
+}
+
+var hours = ['6am', '7am', '8am', '9am', '9am', '10am', '11am','12pm','12pm','1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',]
+
+var seattle = [];
+
+var Stores = function( storeName, minCust, maxCust, avgCookies, ) {
+  this.Name = storeName;
+  this.mnCust = minCust;
+  this.mxCust = maxCust;
+  this.avCookies = avgCookies;
+  this.custsEachHour = [];
+  this.cookieEachHour = [];
+  this.total : 0;
+  this.undNumCust = function() {
+    for(var i = 0; i < hours.length; i++){
+      this.custsEachHour.push(random(this.mnCust, this.mxCust));
+    }
+  };
+  this.countCookieEachHour = function() {
+    this.rundNumCust ();
+    for(var i = 0; i < hours.length; i++){
+      var oneHour = Math.ceil(this.custsEachHour[i] * this.avgCookies);
+      this.cookieEachHour.push(oneHour);
+    }
+  };
+  this.remder = function(){
+    var container = document.createElement('div');
+    container.innerHTML = '<p>' + this.Name + '</p>';
+    document.body.appendChild(container);
+
+    var list = document.createElement('ul');
+    var listArr = [];
+    for (var i = 0; i < hours.length; i++) {
+      listArr.push('<li>' + hours[i] + ' : ' + this.cookieEachhour[i] + '</li>');
+    }
+
+    var fullList = listArr.join('');
+    list.innerHTML = fullList;
+    document.body.appendChild(list);
+  };
+seattle.push(remder);
+};
+
+new Stores('1st and pike', 23, 65, 6.3);
+
+
+
+
+
 var firstandPike = {
   minCust : 23,
   maxCust : 65,
   avgCookie : 6.3,
   rundNumCust : function() {
+    this.minCust;
+    this.maxCust;
     min=this.minCust;
     max=this.maxCust;
     return  Math.floor(Math.random() * (max - min)) + min;
@@ -66,7 +120,6 @@ var alki = {
   }
 }
 
-var hours = ['6am', '7am', '8am', '9am', '9am', '10am', '11am','12pm','12pm','1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',]
 
 function makeHTML() {
 
